836cf8c7dfbfdc69245f8cf7e6b472e4dd30002d
danniannie <dannielle.young@hotmail.com>
2019-07-22 16:07:18 +0100

    testing added

--- cypress/integration/nc-news.spec.js
+++ cypress/integration/nc-news.spec.js
@@ -19,83 +19,74 @@ describe('/', () => {
   });
 
   describe('Articles Component', () => {
-    beforeEach(() => {
-      cy.server()
-      cy.route('GET', 'https://nc-news-dy.herokuapp.com/api/articles/', 'fx:articles.json')
 
-    });
-    it('Loads the articles', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=articles]')
-    });
-    it('Article card includes article title', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=articleCard]')
-        .children()
-        .contains('Express')
-    });
-    it('Loads the votes component', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=votes]')
-        .contains('Votes')
-    });
-    it('Article card links to Article Page', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=8]').click()
-      cy.url().should("equal", 'http://localhost:3000/articles/8')
-    });
   });
+  it('Loads the articles', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=articles]')
+  });
+  it('Article card includes article title', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=articleCard]')
+      .children()
+      .contains('Express')
+  });
+  it('Loads the votes component', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=votes]')
+      .contains('Votes')
+  });
+  it('Article card links to Article Page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=8]').click()
+    cy.url().should("equal", 'http://localhost:3000/articles/8')
+  });
+});
 
-  describe('Header component', () => {
-    it('Loads the Header', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=header]')
-        .children()
-        .children()
-        .contains('NC News')
-    });
-    it('Header links to the home page', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=header]').click()
-      cy.url().should("equal", 'http://localhost:3000/')
-    });
+describe('Header component', () => {
+  it('Loads the Header', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=header]')
+      .children()
+      .children()
+      .contains('NC News')
   });
+  it('Header links to the home page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=header]').click()
+    cy.url().should("equal", 'http://localhost:3000/')
+  });
+});
 
-  describe('Sort Block', () => {
-    it('Loads the sort', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy=sort]')
-    });
+describe('Sort Block', () => {
+  it('Loads the sort', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-cy=sort]')
   });
+});
 
-  describe('/articles/*', () => {
-    beforeEach(() => {
-      cy.server()
-      cy.route('GET', 'https://nc-news-dy.herokuapp.com/api/articles/33', 'fx:singleArticles.json')
-      cy.route('GET', 'https://nc-news-dy.herokuapp.com/api/articles/33/comments', 'fx:comments.json')
-    });
-    it('Loads article text', () => {
-      cy.visit('http://localhost:3000/articles/33')
-      cy.get('[data-cy=articleBody]')
-        .children()
-        .contains('Seafood substitutions are increasing')
-    });
-    it('Loads add comment box', () => {
-      cy.visit('http://localhost:3000/articles/33')
-      cy.get('[data-cy=addComments')
-    })
-    it('Loads comments', () => {
-      cy.visit('http://localhost:3000/articles/33')
-      cy.get('[data-cy=comments')
-    })
+describe('/articles/*', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route('GET', 'https://nc-news-dy.herokuapp.com/api/articles/33', 'fx:singleArticles.json')
+    cy.route('GET', 'https://nc-news-dy.herokuapp.com/api/articles/33/comments', 'fx:comments.json')
   });
+  it('Loads article text', () => {
+    cy.visit('http://localhost:3000/articles/33')
+    cy.get('[data-cy=articleBody]')
+      .children()
+      .contains('Seafood substitutions are increasing')
+  });
+  it('Loads add comment box', () => {
+    cy.visit('http://localhost:3000/articles/33')
+    cy.get('[data-cy=addComments')
+  })
+  it('Loads comments', () => {
+    cy.visit('http://localhost:3000/articles/33')
+    cy.get('[data-cy=comments')
+  })
+});
 
 });
 
-
-
-
-//votes component, button makes the vote go up and other makes it go down
-//link on the footer goes to correct link
-// 
 
